063613821c800e29b163b971731fb6e832e9244f
Sebastian Romejko <sebastian.romejko@zhp.net.pl>
2024-04-04 00:57:16 +0200

    Fish character and background river

--- assets/fish.gd
+++ assets/fish.gd
@@ -1,6 +1,15 @@
-extends Node2D
+extends CharacterBody2D
 
 @onready var viewport = $sprite/sub_viewport
 
+@export var move_speed: int = 30
+
 func _process(delta):
 	viewport.render_target_update_mode = viewport.UPDATE_ONCE
+	
+	velocity.x = (Input.get_action_strength("right") - Input.get_action_strength("left")) * move_speed
+	velocity.y = (Input.get_action_strength("down") - Input.get_action_strength("up")) * move_speed
+	
+	look_at(global_position - velocity)
+	
+	move_and_slide()
