710b8b53f14e7deefe26d7557dcc61b1e5034096
Sebastian Romejko <sebastian.romejko@zhp.net.pl>
2024-04-06 12:15:02 +0200

    Added lights

--- ui/cutscene_borders.gd
+++ ui/cutscene_borders.gd
@@ -1,6 +1,11 @@
 extends Control
 
+@export var border_height: int = 20
+
 func _ready():
 	var tween = get_tree().create_tween()
-	tween.tween_property($upper_border, "custom_minimum_size.y", 30, 2)
-	tween.tween_property($bottom_border, "custom_minimum_size.y", 30, 2)
+	tween.tween_property($upper_border, "custom_minimum_size", Vector2(0, border_height), 1)
+	var tween2 = get_tree().create_tween()
+	tween2.tween_property($bottom_border, "custom_minimum_size", Vector2(0, border_height), 1)
+	tween.play()
+	tween2.play()
